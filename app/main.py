c2a16d606d76bbdd71eec7595b9b4ae69541dff2
Tooba Qamar <104388267+Tooba187@users.noreply.github.com>
2025-04-21 15:04:57 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from app.file_parser import extract_text
-from app.quiz_generator import generate_quiz
+from file_parser import extract_text
+from quiz_generator import generate_quiz
 
 st.set_page_config(page_title="AI Quiz Generator", layout="centered")
 st.title("AI Quiz Generator")
